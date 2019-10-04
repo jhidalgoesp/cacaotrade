06b05049c75aa29dcc842bf1d0e567b72e4e46bf
Alec Trievel <atrievel@ceiamerica.com>
2019-10-04 10:43:20 -0400

    Add CORS config and functionality

--- config.js
+++ config.js
@@ -6,5 +6,16 @@ module.exports = {
   dbPassword: process.env.DB_PASS,
   dbHost: process.env.DB_HOST,
   dbPort: process.env.DB_PORT,
-  dbName: process.env.DB_NAME
+  dbName: process.env.DB_NAME,
+  corsConfiguration: {
+    delimiter: process.env.ORIGIN_DELIMITER || ',',
+    allowedOrigins: process.env.ALLOWED_ORIGINS || '*',
+    allowedMethods: process.env.ALLOWED_METHODS || '',
+    allowedHeaders: process.env.ALLOWED_HEADERS || '',
+    exposedHeaders: process.env.EXPOSED_HEADERS || '',
+    allowCredentials: (process.env.ALLOW_CREDENTIALS || false) == 'true',
+    maxAge: Number.parseInt(process.env.MAX_AGE || 0),
+    preflightContinue: (process.env.PREFLIGHT_CONTINUE || false) == 'true',
+    responseSuccessCode: Number.parseInt(process.env.OPTIONS_SUCCESS_STATUS_CODE || 204)
+  }
 };
